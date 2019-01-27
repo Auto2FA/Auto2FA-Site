37d944a236def2faf760391208d24c34b59c7821
Aspecting <mrmyers2k@gmail.com>
2019-01-27 07:39:45 -0600

    Preparation for email authentication

--- routes/routers.js
+++ routes/routers.js
@@ -5,6 +5,7 @@ module.exports = function(app, passport, bruteforce) {
     var url = require('url');
     var qs = require('qs');
     var twoFactor = require('node-2fa');
+    var crypto = require('crypto');
     var tempRoutes = {};
 
     app.get('/', function(req, res) {
@@ -14,9 +15,9 @@ module.exports = function(app, passport, bruteforce) {
     });
 
     app.get('/login', function(req, res) {
-        res.render('login.ejs', { message: req.flash('loginMessage') });
+        res.render('login.ejs', { smessage: req.flash('loginMessage'), emessage: req.flash('loginMessage') });
     });
-    app.post('/login', bruteforce.prevent, function(req, res) {
+    app.post('/login', /*bruteforce.prevent,*/ function(req, res) {
         var parsedUrl = qs.parse(url.parse(req.url).query);    
         var email = parsedUrl.email || req.body.email;
         User.findOne({ 'email' :  email }, function(err, user) {
@@ -27,7 +28,7 @@ module.exports = function(app, passport, bruteforce) {
                 newUser.email = email;
                 secretObj = twoFactor.generateSecret({name: 'Auto2FA', account: email});
                 secretQr = secretObj.qr;
-                newUser.secretObj.secret = secret;
+                newUser.secret = secretObj.secret;
                 newUser.save(function(err, user) {
                     if(err) {
                         res.status(500).send('Something broke! ' + err);
@@ -39,10 +40,11 @@ module.exports = function(app, passport, bruteforce) {
             var randomID = crypto.randomBytes(20).toString('hex');
             // TODO: Send e-mail with /auth/{randomID}
             // If qr code isn't equal to "" then display the QR code in the e-mail
-            tempRoutes[irandomID] = function(req, res) {
+            tempRoutes[randomID] = function(req, res) {
                 // TODO: Use TCP server to connect to client at port
                 res.render('auth/wait.ejs');
             };
+            res.render('login.ejs', { smessage: 'Check ' + email + " for your sign in link" });
         });
     });
 
@@ -57,15 +59,6 @@ module.exports = function(app, passport, bruteforce) {
             res.status(404).send('Not found');
         }
     });
-
-    app.get('/signup', function(req, res) {
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
-    app.post('/signup', bruteforce.prevent, passport.authenticate('signup', {
-      successRedirect : '/profile',
-      failureRedirect : '/signup',
-      failureFlash : true
-    }));
 
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
